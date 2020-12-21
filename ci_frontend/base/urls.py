05c7d67b40d1daeaf364321f59536c22b4d4c467
rahul18cracker <rahul18cracker@gmail.com>
2020-12-21 07:46:18 -0800

    Things done: Cleaned up the non needed older search and display code, moved everthing to AJAX based search. NEXT Steps: 1. need to look at what financial analytics to add, need to study what all to show(study and apply)

--- ci_frontend/base/urls.py
+++ ci_frontend/base/urls.py
@@ -3,17 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # This is the results page after the search is done
-    path('search/',
-         views.SearchResultsView.as_view(),
-         #name='common_stock_search_results'),
-         name='companies'),
-
-    # Landing page with search bar fpr the user
+    # Landing page with search bar for the user
     path('',
          views.CommonStockSearchPageView.as_view(),
          name='companies'),
-    path('company/',
-         views.ajax_view,
-         name="companies")
 ]
